c46baee384ca4fdb2f005d9bef17102970577e87
Fidel Collin <fidelcollin@icloud.com>
2021-02-15 20:46:52 +0000

    added toggleTheme function conditions, adjusted styling for light and dark theme in index.css. Added localStorage to keep the state of theme change on refresh of page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Article from "./components/Article";
 import { posts } from "./data";
 
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
 function App() {
   const [toggle, setToggle] = useState(true);
+  const [theme, setTheme] = useState(getStorageTheme());
 
   const toggleTheme = () => {
     if (toggle) {
@@ -12,7 +21,16 @@ function App() {
     } else {
       setToggle(true);
     }
+
+    theme === "light-theme" ? setTheme("dark-theme") : setTheme("light-theme");
+
+    return theme;
   };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   return (
     <main>
